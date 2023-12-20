0b55cbafa6882a68157d32c0e76c768b79f2f3d6
Naa Kai <naa.kai.odametey@gmail.com>
2023-12-20 10:04:12 +0000

    renamed tradecpncept to trades, remove, trades

--- domain-model/src/TraderX/Trades/Trades.elm
+++ domain-model/src/TraderX/Trades/Trades.elm
@@ -3,6 +3,12 @@ module TraderX.Trades.Trades exposing (..)
 import TraderX.Account.Accounts exposing (AccountId)
 import TraderX.Positions.Positions exposing (Position)
 import TraderX.Types exposing (Date, ID, Quantity, Security)
+import TraderX.Stock.Stock exposing (Stock)
+import TraderX.Account.Accounts exposing (Account)
+import TraderX.Account.Accounts exposing (findAccountBy)
+import TraderX.Account.Accounts exposing (AccountError)
+import TraderX.Stock.Stock exposing (StockError)
+import TraderX.Stock.Stock exposing (findStockBy)
 
 type alias State =
     String
@@ -28,25 +34,25 @@ type alias TradeBookingResult =
     , position : Position
     }
 
-type Ticker
-    = TickerInvalid
-    | TickerValid Security
+-- type Ticker
+--     = TickerInvalid
+--     | TickerValid Security
 
-type Account
-    = AccountInvalid 
-    | AccountValid AccountId
+-- type Account
+--     = AccountInvalid 
+--     | AccountValid AccountId
 
 type ResourceNotFound
     = AccountNotFound
     | TickerNotFound
 
-type alias TradeOrderService =
-    { id : ID
-    , security : Ticker
-    , quantity : TradeQuantity
-    , accountId : Account
-    , side : TradeSide
-    }
+-- type alias TradeOrderService =
+--     { id : ID
+--     , security : Ticker
+--     , quantity : TradeQuantity
+--     , accountId : Account
+--     , side : TradeSide
+--     }
 
 type alias Trade =
     { id : ID
@@ -63,34 +69,62 @@ type alias TradeOrder =
     { id : ID
     , state : State
     , security : Security
-    , quantity : TradeQuantity
+    , quantity : Quantity
     , accountId : AccountId
     , side : TradeSide
     }
 
-createTradeOrder : TradeOrderService -> Result ResourceNotFound TradeOrderService
-createTradeOrder tradeOrder =
-   let
-       validTicker :  Result ResourceNotFound TradeOrderService
-       validTicker =
-           case tradeOrder.security of
-               TickerValid _ ->
-                   Ok tradeOrder
-               TickerInvalid ->
-                   Err TickerNotFound
-                   
-       validAccount :  Result ResourceNotFound TradeOrderService
-       validAccount =
-           case tradeOrder.accountId of
-               AccountValid _ ->
-                   Ok tradeOrder
-               AccountInvalid ->
-                   Err AccountNotFound
+validateTradeOrder : TradeOrder -> List Stock -> List Account -> Result ResourceNotFound TradeOrder
+validateTradeOrder tradeOrder stocks accounts =
+    if validateTicker tradeOrder.security stocks == False then
+        Err TickerNotFound
+    else if validateAccount tradeOrder.accountId accounts == False then
+        Err AccountNotFound
+    else
+        Ok tradeOrder
 
-   in
-   case validTicker of
-       Err _ -> validTicker
-       Ok _ -> validAccount
+
+--    let
+--        validTicker :  Result ResourceNotFound TradeOrderService
+--        validTicker =
+--            case tradeOrder.security of
+--                TickerValid _ ->
+--                    Ok tradeOrder
+--                TickerInvalid ->
+--                    Err TickerNotFound
+                   
+--        validAccount :  Result ResourceNotFound TradeOrderService
+--        validAccount =
+--            case tradeOrder.accountId of
+--                AccountValid _ ->
+--                    Ok tradeOrder
+--                AccountInvalid ->
+--                    Err AccountNotFound
+
+--    in
+--    case validTicker of
+--        Err _ -> validTicker
+--        Ok _ -> validAccount
+
+validateAccount : AccountId -> List Account -> Bool
+validateAccount accountId accountList =
+    let
+        account : Result AccountError Account
+        account = findAccountBy accountId accountList
+    in
+        case account of
+            Err _ -> False
+            Ok _ -> True
+
+validateTicker : Security -> List Stock -> Bool
+validateTicker security stockList =
+    let
+        stock : Result StockError Stock
+        stock = findStockBy security stockList
+    in
+        case stock of
+            Err _ -> False
+            Ok _ -> True
 
 
 calculateQuantity: TradeSide -> Int -> Int
